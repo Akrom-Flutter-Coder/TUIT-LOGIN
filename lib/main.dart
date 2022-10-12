330153cfbace8eec1a13cbf045847c4509751e94
Akromjon Abdugafforov <112075678+Akrom-Flutter-Coder@users.noreply.github.com>
2022-10-12 22:56:49 +0500

    by Akrom
    Changed

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue
       ),
-      home: GeInformation()
+      home: HomePage()
     );
   }
 }
